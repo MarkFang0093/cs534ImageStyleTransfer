180412f668d728d9c38d446fcd5143e51e404d86
Mark Fang <markfangmarcus@outlook.com>
2018-12-12 15:29:39 -0600

    update

--- js/scripts.js
+++ js/scripts.js
@@ -164,7 +164,7 @@
 	   
        /* Load More Post */	
 	   	
-        $("div.blog-post").slice(0, 4).show();
+        $("div.blog-post").slice(0, 2).show();
           $("#load-more-post").on('click', function (e) {
              e.preventDefault();
              $("div.blog-post:hidden").slice(0, 1).slideDown(300);
